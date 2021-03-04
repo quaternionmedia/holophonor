c8773838a693fe1e750579e8f8ee4b04d17e6443
Harpo <harpo@harpo.me>
2021-03-04 14:48:48 -0800

    working launchpadX plugin!!

--- holophonor/launchpadX.py
+++ holophonor/launchpadX.py
@@ -1,4 +1,27 @@
-import holophonor
+from holophonor import holoimpl
+from holophonor.holospecs import Holophonor
+from rtmidi.midiutil import open_midioutput
+from rtmidi.midiconstants import NOTE_ON
 
-class LaunchpadX:
-    
+
+class LaunchpadX(Holophonor):
+    def __init__(self, port, plugins=[]):
+        super().__init__()
+        self.port = port
+        self.midi, self.name = open_midioutput(self.port, client_name='holo->launchpadX')
+        self.plugins = plugins
+        self.live = False
+        self.toggleLive()
+        self.map = []
+        n = 81
+        for y in range(4):
+            for x in range(8):
+                self.map.append(n + x)
+            n -= 10
+    def toggleLive(self):
+        # switch to / from programming / Live mode
+        self.midi.send_message([240, 0, 32, 41, 2, 12, 14, 0 if self.live else 1, 247])
+        self.live = not self.live
+    @holoimpl
+    def triggerLoop(self, loop, volume):
+        self.midi.send_message([NOTE_ON, self.map[loop - 1], volume])
--- holophonor/main.py
+++ holophonor/main.py
@@ -1,6 +1,6 @@
 from holophonor.holospecs import Holophonor
 from holophonor.lib import Fweelin
-
+from holophonor.launchpadX import LaunchpadX
 from pluggy import PluginManager
 
 
@@ -14,6 +14,8 @@ def get_plugin_manager():
     pm = PluginManager('holophonor')
     pm.add_hookspecs(Holophonor)
     pm.register(Fweelin('FreeWheeling:FreeWheeling IN 1'))
+    pm.register(LaunchpadX('Launchpad X:Launchpad X MIDI 2'))
+
     return pm
 
 
