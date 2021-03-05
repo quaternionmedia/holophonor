0a743e9a9998bdf94a4a46d6c804a077755193c6
Harpo <harpo@harpo.me>
2021-03-04 19:17:00 -0800

    working callback on launchpad!

--- holophonor/launchpadX.py
+++ holophonor/launchpadX.py
@@ -1,6 +1,6 @@
 from holophonor import holoimpl
 from holophonor.holospecs import Holophonor
-from rtmidi.midiutil import open_midioutput
+from rtmidi.midiutil import open_midiinput
 from rtmidi.midiconstants import NOTE_ON
 
 
@@ -15,6 +15,8 @@ class LaunchpadX(Holophonor):
             for x in range(8):
                 self.map.append(n + x)
             n -= 10
+        self.input, self.input_name = open_midiinput(self.port)
+        self.input.set_callback(self)
     def toggleLive(self):
         # switch to / from programming / Live mode
         self.midi.send_message([240, 0, 32, 41, 2, 12, 14, 0 if self.live else 1, 247])
@@ -28,3 +30,11 @@ class LaunchpadX(Holophonor):
     @holoimpl
     def triggerLoop(self, loop, volume):
         self.midi.send_message([NOTE_ON, self.map[loop - 1], volume])
+    
+    def __call__(self, event, data=None):
+        message, deltatime = event
+        print(message)
+        if message[0] == NOTE_ON:
+            if message[1] in self.map:
+                l = self.map.index(message[1])
+                self.hook.triggerLoop(loop=l, volume=message[2])
