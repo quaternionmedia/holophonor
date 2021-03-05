2139240e17e9f5c55c0ad7ff6e2a6c6cefc3b7fc
Harpo <harpo@harpo.me>
2021-03-04 18:37:04 -0800

    move port opening to Holophonor class

--- holophonor/holospecs.py
+++ holophonor/holospecs.py
@@ -1,9 +1,13 @@
 from pluggy import HookspecMarker
+from rtmidi.midiutil import open_midioutput
 
 holospec = HookspecMarker('holophonor')
 
 class Holophonor:
-    def __init__(self):
+    def __init__(self, hook, port, plugins=[]):
+        self.port = port
+        self.midi, self.name = open_midioutput(self.port, client_name='holo->fweelin')
+        self.plugins = plugins
         self.loops = [None]*32
         self.scenes = [None]*8
     
