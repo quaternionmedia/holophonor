4daff634f11b004686ef1403378908944cb31f00
Harpo <harpo@harpo.me>
2021-03-04 19:16:38 -0800

    added additional holospecs

--- holophonor/holospecs.py
+++ holophonor/holospecs.py
@@ -5,16 +5,45 @@ holospec = HookspecMarker('holophonor')
 
 class Holophonor:
     def __init__(self, hook, port, plugins=[]):
+        self.hook = hook
         self.port = port
         self.midi, self.name = open_midioutput(self.port, client_name='holo->fweelin')
         self.plugins = plugins
         self.loops = [None]*32
         self.scenes = [None]*8
+        self.current_scene = None
+        self.shift = False
+        self.mutes = [False]*4
+        self.overdub = False
+        self.cut = False
+        self.tap = False
     
     @holospec
     def triggerLoop(self, loop: int, volume: int):
-        """trigger loop at volume"""
+        '''trigger loop at volume'''
+    
+    @holospec
+    def triggerScene(self, scene: int):
+        '''trigger a scene'''
+        
+    @holospec
+    def eraseLoop(self, loop: int):
+        '''erase the selected loop'''
+    
+    @holospec
+    def clear(self):
+        '''clear all loops'''
+    
+    @holospec
+    def toggleOverdub(self, overdub=True):
+        '''set overdub mode'''
+    
+    @holospec
+    def toggleCut(self, cut=True):
+        '''toggle cut mode'''
     
     @holospec
     def close(self):
-        """close the connection"""
+        '''close the connection'''
+    
+    
