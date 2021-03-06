1b1afd2e7bd5424e37f311b467790d4d8c895e02
Harpo <harpo@harpo.me>
2021-03-05 20:56:26 -0800

    added overdubLoop and startLoopInCutMode

--- holophonor/freewheeling.py
+++ holophonor/freewheeling.py
@@ -21,30 +21,40 @@ class Fweelin(Holophonor):
         self.playLoop(loop, 127)
     
     @holoimpl
+    def startLoopInCutMode(self, loop: int, volume: int):
+        self.midi.send_message([CONTROL_CHANGE, 118, 0])
+        self.midi.send_message([NOTE_ON, loop, volume])
+        self.midi.send_message([CONTROL_CHANGE, 118, 127])
+    
+    @holoimpl
+    def overdubLoop(self, loop: int):
+        self.playLoop(loop, 127)
+        
+    @holoimpl
     def toggleShift(self):
         self.midi.send_message([CONTROL_CHANGE, 98, 0 if self.shift else 127])
         self.shift = not self.shift
-
+    
     @holoimpl
     def toggleCut(self):
         self.midi.send_message([CONTROL_CHANGE, 96, 0 if self.cut else 127])
         self.cut = not self.cut
-
+    
     @holoimpl
     def toggleOverdub(self):
         self.midi.send_message([CONTROL_CHANGE, 97, 0 if self.overdub else 127])
         self.overdub = not self.overdub
-
-
+    
     @holoimpl
     def deletePulse(self):
         self.midi.send_message([CONTROL_CHANGE, 108, 0])
-
+    
     @holoimpl
     def tapPulse(self):
         self.midi.send_message([CONTROL_CHANGE, 95, 127])
-
+    
     @holoimpl
     def toggleMute(self, channel: int):
         self.midi.send_message([CONTROL_CHANGE, 56  + channel, 0 if self.mutes[channel] else 127])
         self.mutes[channel] = not self.mutes[channel]
+    
