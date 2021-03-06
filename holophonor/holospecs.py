44ce5324a9b824226c02a22be36d6549a047f675
Harpo <harpo@harpo.me>
2021-03-05 17:49:36 -0800

    added holospecs for all functions used in midihub!!!

--- holophonor/holospecs.py
+++ holophonor/holospecs.py
@@ -19,29 +19,94 @@ class Holophonor:
         self.tap = False
     
     @holospec
-    def triggerLoop(self, loop: int, volume: int):
-        '''trigger loop at volume'''
+    def playLoop(self, loop: int, volume: int):
+        '''play loop at volume'''
     
     @holospec
-    def triggerScene(self, scene: int):
-        '''trigger a scene'''
-        
+    def stopLoop(self, loop: int):
+        '''pause loop'''
+    
+    @holospec
+    def recordLoop(self, loop: int):
+        '''record loop'''
+    
     @holospec
     def eraseLoop(self, loop: int):
         '''erase the selected loop'''
     
     @holospec
-    def clear(self):
-        '''clear all loops'''
+    def clearLoop(self, loop: int):
+        '''clear the button on release for the selected loop'''
     
     @holospec
-    def toggleOverdub(self, overdub=True):
-        '''set overdub mode'''
+    def startLoopInCutMode(self, loop: int, volume: int):
+        '''fix for FreeWheeling not starting loops while in cut mode'''
     
     @holospec
-    def toggleCut(self, cut=True):
+    def overdubLoop(self, loop: int):
+        '''overdub the selected loop'''
+        
+    @holospec
+    def recallScene(self, scene: int):
+        '''recall a scene'''
+    
+    @holospec
+    def storeScene(self, scene: int):
+        '''store a scene'''
+    
+    @holospec
+    def eraseScene(self, scene: int):
+        '''erase a scene'''
+    
+    @holospec
+    def clearScene(self, scene: int):
+        '''release the scene button'''
+    
+    @holospec
+    def toggleShift(self):
+        '''toggle shift mode'''
+    
+    @holospec
+    def toggleCut(self):
         '''toggle cut mode'''
     
+    @holospec
+    def toggleOverdub(self):
+        '''toggle overdub mode'''
+    
+    @holospec
+    def deletePulse(self):
+        '''delete pulse'''
+    
+    @holospec
+    def clearPulse(self):
+        '''release pulse button'''
+    
+    # @holospec
+    # def clear(self):
+    #     '''clear all loops'''
+    
+    @holospec
+    def tapPulse(self):
+        '''tap pulse'''
+    
+    @holospec
+    def toggleMute(self, channel: int):
+        '''toggle mute on channel'''
+    
+    @holospec
+    def toggleFX(self, fx: int):
+        '''toggle fx'''
+    
+    @holospec
+    def setDrumPatch(self, patch: int):
+        '''set drum pad patch'''
+    
+    @holospec
+    def setDrumBank(self, patch: int):
+        '''set drum pad bank'''
+    
+
     @holospec
     def close(self):
         '''close the connection'''
