b607e9ee211391a5855566c725295c1ad14fd4b5
Harpo <harpo@harpo.me>
2021-03-05 18:22:17 -0800

    basic working Fweelin play hooks!

--- holophonor/lib.py
+++ holophonor/lib.py
@@ -1,12 +1,24 @@
 from holophonor import holoimpl
-from holospecs import Holophonor
+from holophonor.holospecs import Holophonor
 from rtmidi.midiconstants import NOTE_ON, NOTE_OFF, POLY_AFTERTOUCH, CONTROL_CHANGE, PROGRAM_CHANGE
 
 
 class Fweelin(Holophonor):
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
+    @holoimpl
+    def playLoop(self, loop: int, volume: int):
+        self.midi.send_message([NOTE_ON, loop, volume])
         
+    @holoimpl
+    def stopLoop(self, loop: int):
+        self.playLoop(loop, 127)
+
+    @holoimpl
+    def recordLoop(self, loop: int):
+        self.playLoop(loop, 127)
+
+    @holoimpl
+    def eraseLoop(self, loop: int):
+        self.playLoop(loop, 127)
         
         
     @holoimpl
