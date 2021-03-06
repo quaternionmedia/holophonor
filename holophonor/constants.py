5ad55916660685d657b310fda353444f64cd2067
Harpo <harpo@harpo.me>
2021-03-05 18:42:41 -0800

    added constants file

---
+++ holophonor/constants.py
@@ -0,0 +1,9 @@
+EMPTY = 0 # black
+RECORDING = 5 # red
+PLAYING = 21 # green
+STOPPED = 43 # navy
+CUT = 13 # cut mode - yellow
+ERASE = 84 # orange
+PULSE = 55 # dim pink
+TAP = 53 # bright pink
+GREEN = [ 123, 23, 64, 22, 76, 87, 21, 122 ]
