fcf70245a4b1f5daf4217b429d210c044fecdedf
Harpo <harpo@harpo.me>
2021-06-02 11:53:02 -0700

    fixed qsynth reference

--- holophonor/qsynth.py
+++ holophonor/qsynth.py
@@ -1,9 +1,10 @@
 from holophonor import holoimpl
-from holophonor.launchpadX import DRUM_PATCH_COLORS
 from rtmidi.midiutil import open_midioutput
 from rtmidi.midiconstants import PROGRAM_CHANGE
 
 class Qsynth:
+    DRUM_PATCH_COLORS = [32, 24, 25, 56]
+
     def __init__(self, hook, port, client_name='qsynth', **kwargs):
         self.hook = hook
         self.midi, self.name = open_midioutput(port, client_name=client_name)
@@ -14,4 +15,4 @@ class Qsynth:
     
     @holoimpl
     def setDrumPatch(self, patch: int):
-        self.midi.send_message([PROGRAM_CHANGE | 0x9, DRUM_PATCH_COLORS[patch]])
+        self.midi.send_message([PROGRAM_CHANGE | 0x9, self.DRUM_PATCH_COLORS[patch]])
