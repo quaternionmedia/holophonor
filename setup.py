de967fb8578d82da3627e3d852409162b5bdfe8a
Harpo <harpo@harpo.me>
2022-09-17 08:32:10 -0700

    Update install_requires

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holophonor',
-    install_requires='pluggy>=1.0,<1.1',
+    install_requires=['pluggy >= 1.0, < 1.1', 'python-rtmidi >= 1.4.9, < 1.5'],
     entry_points={'console_scripts': ['holophonor=holophonor.main:main']},
     packages=find_packages(),
 )
